b542c9de0eee89bf4015429b6eb65d10d9388906
charlybanya <charlybanya@gmail.com>
2014-08-28 16:22:41 -0500

    Primera Función

--- includes/Database.php
+++ includes/Database.php
@@ -6,9 +6,16 @@
  * @author root
  */
 class Database {
-    public $a = "cadena";
-    function createConnection() {
-        $mysqli = new mysqli("localhost", "root", "MeRc891026*", "inviteme");
-        return $mysqli;
+
+    public function createConnection() {
+        $connectData = array(host => 'localhost', usr => 'root', pwd => 'MeRc891026*', db => 'inviteme');
+        $mysqli = new mysqli($connectData['host'], $connectData['usr'], $connectData['pwd'], $connectData['db']);
+        if ($mysqli->connect_errno) {
+            die("Fallo al conectar a la Base de Datos: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+        } else {
+            echo "Exito!";
+            return $mysqli;
+        }
     }
+
 }
