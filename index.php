1492d03cf11bd7c01d25c6b68da0d25d2c7279e5
samsung <samsung@samsung-laptop>
2014-09-25 10:11:06 -0500

    Mockup!!

--- index.php
+++ index.php
@@ -33,10 +33,13 @@ use Facebook\GraphUser;
 <html>
     <head>
         <meta charset="UTF-8">
-        <link href="css/colorbox.css" rel="stylesheet" type="text/css">
-        <script src="js/jquery-1.11.0.min.js"></script>
-        <script src="js/jquery.form.min.js"></script>
-        <script src="js/jquery.colorbox-min.js"></script>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>www.prickie.com.mx</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style4.css" />
+        <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
         <script src="js/inviteme.js"></script>
         <title>www.prickie.com.mx</title>
     </head>
@@ -44,7 +47,7 @@ use Facebook\GraphUser;
         <?php
         FacebookSession::setDefaultApplication('848341915184985', '746f5977bece9cfe41956dd3a22877f6');
         $helper = new FacebookRedirectLoginHelper('http://www.telmexhub.com.mx/inviteme/fbRedirect.php', '848341915184985', '746f5977bece9cfe41956dd3a22877f6');
-        echo '<a href="' . $helper->getLoginUrl(array('scope' => 'email')) . '" >Login</a>';
+        echo '<a class="fb-connect button" href="' . $helper->getLoginUrl(array('scope' => 'email')) . '" ></a>';
         ?>
     </body>
 </html>
